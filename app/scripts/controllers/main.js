d484ca2603b59443954c9cda50f1812ae39b28bb
mdeumie <2m1tema@gmail.com>
2015-02-04 15:49:31 +0100

    css + get index on memory click

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,18 +8,22 @@
  * Controller of the memorydiaApp
  */
 angular.module('memorydiaApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.newMemoryTitle = '';
-    $scope.newMemoryContent = '';
-    $scope.memories = [
-        {title: 'memory1', content: 'content of memory1'},
-        {title: 'memory2', content: 'content of memory2'},
-        {title: 'memory3', content: 'content of memory3'},
-        {title: 'memory4', content: 'content of memory4'},
-        {title: 'memory5', content: 'content of memory5'}
-    ];
+    .controller('MainCtrl', function ($scope) {
+        $scope.newMemoryTitle = '';
+        $scope.newMemoryContent = '';
+        $scope.memories = [
+            {title: 'memory1', content: 'content of memory1'},
+            {title: 'memory2', content: 'content of memory2'},
+            {title: 'memory3', content: 'content of memory3'},
+            {title: 'memory4', content: 'content of memory4'},
+            {title: 'memory5', content: 'content of memory5'}
+        ];
 
-        $scope.addMemory = function() {
+        $scope.addMemory = function () {
             $scope.memories.push({title: $scope.newMemoryTitle, content: $scope.newMemoryContent});
-        }
-  });
+        };
+
+        $scope.openMemory = function(memoryIndex) {
+            console.log($scope.memories[memoryIndex]);
+        };
+    });
